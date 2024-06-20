e1df9232783f55dbd9abe05394c3809ff70203ed
Caue Jesus <cs.jesus@gmail.com>
2024-06-20 17:47:53 -0300

    Configuração do prisma.service

--- src/module/tipo-instituicao/tipo-instituicao.module.ts
+++ src/module/tipo-instituicao/tipo-instituicao.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TipoInstituicaoService } from './tipo-instituicao.service';
 import { TipoInstituicaoController } from './tipo-instituicao.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [TipoInstituicaoController],
-  providers: [TipoInstituicaoService],
+  providers: [TipoInstituicaoService, PrismaService],
 })
 export class TipoInstituicaoModule {}

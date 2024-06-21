e8c3a867ac3dc0eb8c2cb2deafc9ad5d2773357a
Caue Jesus <cs.jesus@gmail.com>
2024-06-21 15:57:46 -0300

    Criação module/cidade

--- src/module/cidade/cidade.module.ts
+++ src/module/cidade/cidade.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CidadeService } from './cidade.service';
 import { CidadeController } from './cidade.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [CidadeController],
-  providers: [CidadeService],
+  providers: [CidadeService, PrismaService],
 })
 export class CidadeModule {}
--- src/module/tipo-estabelecimento/tipo-estabelecimento.module.ts
+++ src/module/tipo-estabelecimento/tipo-estabelecimento.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TipoEstabelecimentoService } from './tipo-estabelecimento.service';
 import { TipoEstabelecimentoController } from './tipo-estabelecimento.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [TipoEstabelecimentoController],
-  providers: [TipoEstabelecimentoService],
+  providers: [TipoEstabelecimentoService, PrismaService],
 })
 export class TipoEstabelecimentoModule {}

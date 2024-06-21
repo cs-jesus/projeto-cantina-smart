3f30277a5455636e552b438d60e370a5efa25f64
Caue Jesus <cs.jesus@gmail.com>
2024-06-21 12:57:47 -0300

    Fix instituicao.dto

--- src/module/instituicao/dto/create-instituicao.dto.ts
+++ src/module/instituicao/dto/create-instituicao.dto.ts
@@ -1,10 +1,11 @@
-import { IsString } from "class-validator";
+import { IsOptional, IsString } from "class-validator";
 import { Instituicao } from "../entities/instituicao.entity";
 
 export class CreateInstituicaoDto extends Instituicao{
     @IsString()
     nome: string;
 
+    @IsOptional()
     @IsString()
     sigla: string;
 }

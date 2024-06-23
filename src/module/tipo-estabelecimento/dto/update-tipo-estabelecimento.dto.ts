d37d075f190c40327bef6e15129e71d0977c7656
Caue Jesus <cs.jesus@gmail.com>
2024-06-22 21:47:26 -0300

    Exclusao do modulo 'tipo-estabelecimento'

--- src/module/tipo-estabelecimento/dto/update-tipo-estabelecimento.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTipoEstabelecimentoDto } from './create-tipo-estabelecimento.dto';
-
-export class UpdateTipoEstabelecimentoDto extends PartialType(CreateTipoEstabelecimentoDto) {}

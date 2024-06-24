4b31b424332b90f114c60c859a6f3cc824193664
Caue Jesus <cs.jesus@gmail.com>
2024-06-24 20:49:13 -0300

    Fix

--- src/module/tipo-instituicao/dto/create-tipo-instituicao.dto.ts
+++ src/module/tipo-instituicao/dto/create-tipo-instituicao.dto.ts
@@ -1,4 +1,4 @@
-import { IsAlpha, IsNotEmpty, IsString, Length } from "class-validator";
+import { IsAlpha, IsNotEmpty, IsString, Length, Matches } from "class-validator";
 import { TipoInstituicao } from "../entities/tipo-instituicao.entity";
 
 const minL = 3, maxL = 32;
@@ -9,4 +9,10 @@ export class CreateTipoInstituicaoDto extends TipoInstituicao {
     @Length(minL, maxL, { message: `O campo deve conter entre ${minL} e ${maxL} caracteres.` })
     @IsAlpha('pt-BR', { message: "O campo deve conter somente letras" })
     descricao: string;
+
+    @IsNotEmpty({ message: "O campo não pode estar vazio" })
+    @IsString({ message: "O tipo de dado deve ser uma string" })
+    @Length(minL, maxL, { message: `O campo deve conter entre ${minL} e ${maxL} caracteres.` })
+    @IsAlpha('pt-BR', { message: "O campo deve conter somente letras" })
+    algumacoisa: string;
 }

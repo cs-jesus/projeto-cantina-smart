0b2f8524244bbb6480996137d17413541eff6d9b
cs-jesus <cs.jesus98@gmail.com>
2024-06-26 18:57:50 -0300

    refactor(estabelecimento): Implementação do módulo
    Services, controllers, entity, dtos

--- src/module/estabelecimento/dto/create-estabelecimento.dto.ts
+++ src/module/estabelecimento/dto/create-estabelecimento.dto.ts
@@ -1,1 +1,48 @@
-export class CreateEstabelecimentoDto {}
+import { IsBoolean, IsDate, IsEmail, IsInt, IsNotEmpty, IsNumber, IsOptional, IsString, Matches } from "class-validator";
+import { Estabelecimento } from "../entities/estabelecimento.entity";
+
+export class CreateEstabelecimentoDto extends Estabelecimento {
+
+     
+    @IsString({message: 'o dado recebido dever ser uma string'})
+    @IsNotEmpty({message: 'O campo não pode estar vazio'})
+    @IsDate({message: 'o dado recebido deve ser uma DATA'})
+    @IsNotEmpty({message: 'O campo não pode estar vazio, insita a data do registro'})
+    registro: string | Date;
+
+    @IsString({ message: 'O dado recebido deve ser uma string' })
+    @IsNotEmpty({ message: 'O campo não pode estar vazio' })
+    @Matches(/^\+[0-9]{1,3}\.[0-9]{4,14}(?:x.+)?$/, { message: 'Formato de telefone inválido' })
+    telefone?: string;
+
+    @IsString({ message: 'O dado recebido deve ser uma string' })
+    @IsNotEmpty({ message: 'O campo não pode estar vazio' })
+    @Matches(/^\+[0-9]{1,3}\.[0-9]{4,14}(?:x.+)?$/, { message: 'Formato de telefone inválido' })
+    celular: string;
+
+    @IsNumber({}, { message: 'O dado recebido deve ser um número' })
+    @IsNotEmpty({message: ' O campo não pode estar vazio'})
+    limite?: number;
+
+    @IsNumber({}, { message: 'O dado recebido deve ser um número' })
+    @IsNotEmpty({message: ' O campo não pode estar vazio'})
+    filiais?;
+    //deixei a validação como number para tratar as filiais como "001, 002, 003 " e assim por diante. 
+
+    @IsString({ message: 'O dado recebido deve ser uma string' })
+    @IsNotEmpty({ message: 'O campo não pode estar vazio' })
+    assocProprietario?;
+
+    @IsBoolean({ message: 'O campo deve ser um booleano' })
+    @IsOptional() 
+    fisica?;
+
+    @IsBoolean({ message: 'O campo deve ser um booleano' })
+    @IsOptional()
+    juridica?;
+
+    @IsBoolean({ message: 'O campo deve ser booleano' })
+    @IsOptional()
+    tipoJuridico;
+}
+//deixei de forma booleano para ser true ou false, como vamos ter 3 tipos de verificação, achei que seria o melhor a se fazer
--- src/module/estabelecimento/entities/estabelecimento.entity.ts
+++ src/module/estabelecimento/entities/estabelecimento.entity.ts
@@ -1,1 +1,13 @@
-export class Estabelecimento {}
+import { Prisma } from "@prisma/client";
+
+export class Estabelecimento implements Prisma.EstabelecimentoCreateInput {
+    registro: string | Date;
+    telefone?: string;
+    celular: string;
+    limite?: number;
+    filiais?: Prisma.FilialCreateNestedManyWithoutEstabelecimentoInput;
+    assocProprietario?: Prisma.ProprietarioEstabelecimentoCreateNestedManyWithoutEstabelecimentoInput;
+    fisica?: Prisma.FisicaCreateNestedOneWithoutEstabelecimentosInput;
+    juridica?: Prisma.JuridicaCreateNestedOneWithoutEstabelecimentosInput;
+    tipoJuridico: Prisma.TipoJuridicoCreateNestedOneWithoutEstabelecimentosInput;
+}

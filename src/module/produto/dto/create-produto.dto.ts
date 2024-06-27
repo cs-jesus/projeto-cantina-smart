45c10245c1abeb4467178cae00395336ed4ca5fa
cs-jesus <cs.jesus98@gmail.com>
2024-06-27 20:29:52 -0300

    Fix(module/produto): Revisão e adição de comentários

--- src/module/produto/dto/create-produto.dto.ts
+++ src/module/produto/dto/create-produto.dto.ts
@@ -1,9 +1,15 @@
-import { IsInt, IsNotEmpty, IsString } from "class-validator";
+import { IsInt, IsNotEmpty, IsString, Length } from "class-validator";
 import { Produto } from "../entities/produto.entity";
 
+const nomeMinLenght = 3, nomeMaxLenght = 128;
+
 export class CreateProdutoDto extends Produto {
-    
+
         @IsString({ message: "O tipo de dado deve ser uma string" })
         @IsNotEmpty({ message: "O campo não pode estar vazio" })
+        @Length(nomeMinLenght, nomeMaxLenght, { message: "O campo deve conter entre ${nomeMinLenght} e ${nomeMaxLenght} caracteres." })
+        //Validar caracteres
+
+        //Implementar validação de registro único - Deverá ser solucionado até o final da sprint 3
         nome: string;
 }

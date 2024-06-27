c522a5e0a4d1ff9916a48d858f645cff5b914407
cs-jesus <cs.jesus98@gmail.com>
2024-06-27 19:47:20 -0300

    Fix(endereco.dto): Revisão e adição de comentários

--- src/module/endereco/dto/create-endereco.dto.ts
+++ src/module/endereco/dto/create-endereco.dto.ts
@@ -31,6 +31,8 @@ export class CreateEnderecoDto {
     @Matches(/^[0-9]{1,10}$/, { message: "O campo 'numero' deve conter entre 1 e 10 dígitos numéricos" })
     numero: string;
 
+    //Implementar validação de registro único de CEP + número
+
     @ValidateNested()
     @Type(() => LogradoudoConnectInput)
     @IsNotEmpty({})

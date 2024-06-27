23bc6c4db8b4c4a763d43c7eb61837cf3612bcc2
cs-jesus <cs.jesus98@gmail.com>
2024-06-26 22:27:56 -0300

    feat(cidade.dto): Implementação de regras de validaçao

--- src/module/cidade/dto/create-cidade.dto.ts
+++ src/module/cidade/dto/create-cidade.dto.ts
@@ -1,9 +1,14 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsString, Length, Matches } from "class-validator";
 import { Cidade } from "../entities/cidade.entity";
+
+const nomeMinLenght = 3, nomeMaxLenght = 64;
 
 export class CreateCidadeDto extends Cidade {
 
     @IsString()
-    @IsNotEmpty({ message: "O campo nao pode estar vazio" })
+    @IsNotEmpty()
+    @Length(nomeMinLenght, nomeMaxLenght, { message: `O campo 'nome' deve conter entre ${nomeMinLenght} e ${nomeMaxLenght} caracteres.` })
+    @Matches(/^[\p{L}0-9\s.,-]+$/u, { message: "O nome deve conter apenas letras, números, espaços, pontos e vírgulas" })
+    //Implementar validação de registro único
     nome: string
 }

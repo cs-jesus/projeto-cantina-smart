58c564b3adc71459a381ebe1520e3e37546baf1f
cs-jesus <cs.jesus98@gmail.com>
2024-06-27 19:58:57 -0300

    Revisão e adição de comentário descritivo

--- src/module/endereco/dto/create-endereco.dto.ts
+++ src/module/endereco/dto/create-endereco.dto.ts
@@ -1,27 +1,29 @@
 import { IsInt, IsNotEmpty, IsString, Length, Matches, ValidateNested } from "class-validator";
 import { Type } from "class-transformer";
 
-class LogradoudoConnectInput {
+class LogradoudoConnectInput { //Implementa uma classe com o objetivo de armazenar um valor inteiro e ser posteriormente altera para um Type
     @IsInt({ message: "O campo 'id' deve ser um número inteiro" })
     id: number;
 }
 
-class BairroConnectInput {
+class BairroConnectInput { //Implementa uma classe com o objetivo de armazenar um valor inteiro e ser posteriormente altera para um Type
     @IsInt({ message: "O campo 'id' deve ser um número inteiro" })
     id: number;
 }
 
-class CidadeConnectInput {
+class CidadeConnectInput { //Implementa uma classe com o objetivo de armazenar um valor inteiro e ser posteriormente altera para um Type
     @IsInt({ message: "O campo 'id' deve ser um número inteiro" })
     id: number;
 }
 
-class EstadoConnectInput {
+class EstadoConnectInput { //Implementa uma classe com o objetivo de armazenar um valor inteiro e ser posteriormente altera para um Type
     @IsInt({ message: "O campo 'id' deve ser um número inteiro" })
     id: number;
 }
 
 export class CreateEnderecoDto {
+
+    //Implementa as validações de atributos simples, note que não é realizado validação de fk nesse bloco.
 
     @IsString()
     @Matches(/^[0-9]{8}$/, { message: "O campo 'cep' deve conter exatamente 8 dígitos numéricos" })
@@ -32,6 +34,9 @@ export class CreateEnderecoDto {
     numero: string;
 
     //Implementar validação de registro único de CEP + número
+
+
+    //Implementa validação e restrição dos atributos definidos como chave estrangeira
 
     @ValidateNested()
     @Type(() => LogradoudoConnectInput)

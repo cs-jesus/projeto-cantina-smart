8bfdda30b40034274ed97fb27fc4fbc289020ebb
cs-jesus <cs.jesus98@gmail.com>
2024-06-27 20:33:20 -0300

    Fix(module/unidade-medida): Revisão e adição de comentários

--- src/module/unidade-medida/dto/create-unidade-medida.dto.ts
+++ src/module/unidade-medida/dto/create-unidade-medida.dto.ts
@@ -9,12 +9,12 @@ export class CreateUnidadeMedidaDto extends UnidadeMedida {
   @IsString({ message: "O tipo de dado deve ser uma string" })
   @Length(nomeMinLenght, nomeMaxLenght, { message: "O campo deve conter entre ${nomeMinLenght} e ${nomeMaxLenght} caracteres." })
   @IsAlpha('pt-BR', { message: "O campo deve conter somente letras" })
-  //Implementar validação de registro unico.
+  //Implementar validação de registro unico - Deverá ser solucionado até o final da sprint 3
   nome: string;
 
   @IsString({ message: "O tipo de dado deve ser uma string" })
   @Length(siglaMinLenght, siglaMaxLenght, { message: "O campo deve conter entre ${siglaMinLenght} e ${siglaMaxLenght} caracteres." })
   @IsAlpha('pt-BR', { message: "O campo deve conter somente letras" })
-  //Implementar validação de registro unico.
+  //Implementar validação de registro unico - Deverá ser solucionado até o final da sprint 3
   sigla: string;
 }
--- src/module/unidade-medida/entities/unidade-medida.entity.ts
+++ src/module/unidade-medida/entities/unidade-medida.entity.ts
@@ -1,8 +1,11 @@
 import { Prisma } from "@prisma/client";
 
 export class UnidadeMedida implements Prisma.UnidadeMedidaCreateInput {
+
+    //Definição de atributo básico
     nome: string;
     sigla: string;
 
+    //Declara que a entidade será refereciada como chave estrangeira
     estoque?: Prisma.ProdutoFilialUncheckedCreateNestedManyWithoutUnidadeMedidaInput;
 }

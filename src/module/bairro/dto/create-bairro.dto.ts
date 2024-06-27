407a6c949a72af3abf8ed1e75cddfa8ad20e6a05
cs-jesus <cs.jesus98@gmail.com>
2024-06-26 22:26:42 -0300

    feat(bairroo.dto): Implementação de regras de validaçao

--- src/module/bairro/dto/create-bairro.dto.ts
+++ src/module/bairro/dto/create-bairro.dto.ts
@@ -1,9 +1,14 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsString, Length, Matches } from "class-validator";
 import { Bairro } from "../entities/bairro.entity";
+
+const nomeMinLenght = 3, nomeMaxLenght = 128;
 
 export class CreateBairroDto extends Bairro {
 
     @IsString()
     @IsNotEmpty()
+    @Length(nomeMinLenght, nomeMaxLenght, { message: `O campo 'nome' deve conter entre ${nomeMinLenght} e ${nomeMaxLenght} caracteres.` })
+    @Matches(/^[\p{L}0-9\s.,-]+$/u, { message: "O nome deve conter apenas letras, números, espaços, pontos e vírgulas" })
+    //Implementar validação de registro único
     nome: string
 }

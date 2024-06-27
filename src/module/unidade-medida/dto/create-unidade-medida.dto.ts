23ad0295932b6fdc1fa15e3c07c83e69587ba307
cs-jesus <cs.jesus98@gmail.com>
2024-06-27 11:41:09 -0300

    test(unidade-medida.dto): Teste de implementação de validação

--- src/module/unidade-medida/dto/create-unidade-medida.dto.ts
+++ src/module/unidade-medida/dto/create-unidade-medida.dto.ts
@@ -1,11 +1,20 @@
-import { IsString } from 'class-validator';
+import { IsAlpha, IsString, Length } from 'class-validator';
 import { UnidadeMedida } from '../entities/unidade-medida.entity';
 
+const nomeMinLenght = 3, nomeMaxLenght = 16;
+const siglaMinLenght = 2, siglaMaxLenght = 4;
+
 export class CreateUnidadeMedidaDto extends UnidadeMedida {
-  
-  @IsString()
+
+  @IsString({ message: "O tipo de dado deve ser uma string" })
+  @Length(nomeMinLenght, nomeMaxLenght, { message: "O campo deve conter entre ${nomeMinLenght} e ${nomeMaxLenght} caracteres." })
+  @IsAlpha('pt-BR', { message: "O campo deve conter somente letras" })
+  //Implementar validação de registro unico.
   nome: string;
 
-  @IsString()
+  @IsString({ message: "O tipo de dado deve ser uma string" })
+  @Length(siglaMinLenght, siglaMaxLenght, { message: "O campo deve conter entre ${siglaMinLenght} e ${siglaMaxLenght} caracteres." })
+  @IsAlpha('pt-BR', { message: "O campo deve conter somente letras" })
+  //Implementar validação de registro unico.
   sigla: string;
 }

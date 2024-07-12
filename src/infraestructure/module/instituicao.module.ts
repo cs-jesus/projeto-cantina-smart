a572e6ec8f4b53d687ea553e43dec54caf574273
Caue Jesus <cs.jesus98@gmail.com>
2024-07-12 12:00:49 -0300

    feat(infra/module): Implementação do módulo da instituição

---
+++ src/infraestructure/module/instituicao.module.ts
@@ -0,0 +1,24 @@
+import { Module } from "@nestjs/common";
+import { PrismaModule } from "../prisma/prisma.module";
+
+import { InstituicaoService } from "src/domain/service/instituicao.service";
+import { InstituicaoController } from "src/interface/controllers/instituicao.controller";
+import { PrismaInstituicaoRepository } from "../repositories/prisma/prisma-instituicao.repository";
+
+@Module({
+    imports: [PrismaModule],
+    exports: [InstituicaoService],
+    controllers: [InstituicaoController],
+
+    providers: [
+        InstituicaoService,
+
+        
+
+        {
+            provide: 'InstituicaoRepository',
+            useClass: PrismaInstituicaoRepository
+        },
+    ],
+
+}) export class InstituicaoModule { }

5845843ca2f31495878d11bea1116379c05472c1
Caue Jesus <cs.jesus98@gmail.com>
2024-07-12 12:07:23 -0300

    refactor(domain/entity): Implementação do modelo atualizado da entidade Cidade

--- src/domain/entities/cidade.entity.ts
+++ src/domain/entities/cidade.entity.ts
@@ -1,10 +1,6 @@
-import { Prisma } from "@prisma/client"
-
-export class Cidade implements Prisma.CidadeCreateInput {
-
-    //Definição de atributo básico
-    nome: string;
-
-    //Declara que a entidade será refereciada como chave estrangeira
-    enderecos?: Prisma.EnderecoUncheckedCreateNestedManyWithoutCidadeInput;
+export class Cidade {
+    constructor(
+        public readonly nome: string,
+        public readonly id?: number,
+    ) { }
 }

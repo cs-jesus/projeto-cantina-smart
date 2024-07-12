72632f76964476650c2672b28cbb1c97c014ece0
Caue Jesus <cs.jesus98@gmail.com>
2024-07-12 11:43:17 -0300

    Delete src/unused/instituicao directory

--- src/unused/instituicao/dto/update-instituicao.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateInstituicaoDto } from './create-instituicao.dto';
-import { IsNotEmpty } from 'class-validator';
-
-export class UpdateInstituicaoDto extends PartialType(CreateInstituicaoDto) {
-    @IsNotEmpty({ message: 'O campo \'fk_tipo\' não pode estar vazio' })
-    fk_tipo: number;
-}

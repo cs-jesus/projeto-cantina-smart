756947b07002776dab191cc932570c7070df3a8c
Caue Jesus <cs.jesus98@gmail.com>
2024-07-12 13:02:06 -0300

    refactor(app.module): Importação dos módulos criados

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,20 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './infraestructure/prisma/prisma.module';
 
 import { TipoInstituicaoModule } from './infraestructure/module/tipo-instituicao.modulo';
+import { InstituicaoModule } from './infraestructure/module/instituicao.modulo';
+
 import { LogradouroModule } from './infraestructure/module/logradouro.module';
+import { BairroModule } from './infraestructure/module/bairro.module';
+import { CidadeModule } from './infraestructure/module/cidade.module';
+import { EstadoModule } from './infraestructure/module/estado.module';
 
 @Module({
     imports: [
         PrismaModule,
 
-        TipoInstituicaoModule,
+        TipoInstituicaoModule, InstituicaoModule, 
 
-        LogradouroModule,
+        LogradouroModule, BairroModule, CidadeModule, EstadoModule,
     ],
 
     controllers: [],

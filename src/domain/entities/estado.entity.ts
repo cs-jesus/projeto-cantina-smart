19e9bbce875168bc7fe7463877f6556f5a5f4dc9
Caue Jesus <cs.jesus98@gmail.com>
2024-07-12 12:08:01 -0300

    refactor(domain/entity): Implementação do modelo atualizado da entidade Estado

--- src/domain/entities/estado.entity.ts
+++ src/domain/entities/estado.entity.ts
@@ -1,10 +1,6 @@
-import { Prisma } from "@prisma/client"
-
-export class Estado implements Prisma.EstadoCreateInput {
-
-    //Definição de atributo básico
-    uf: string;
-
-    //Declara que a entidade será refereciada como chave estrangeira    
-    enderecos?: Prisma.EnderecoCreateNestedManyWithoutEstadoInput;
+export class Estado {
+    constructor(
+        public readonly uf: string,
+        public readonly id?: number,
+    ) { }
 }

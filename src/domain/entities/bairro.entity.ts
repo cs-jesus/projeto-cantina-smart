4e28b031f76ed25b0c6c8ecf15d1ed7c68b26c5b
Caue Jesus <cs.jesus98@gmail.com>
2024-07-12 12:06:13 -0300

    refactor(domain/entity): Implementação do modelo atualizado da entidade Bairro

--- src/domain/entities/bairro.entity.ts
+++ src/domain/entities/bairro.entity.ts
@@ -1,10 +1,6 @@
-import { Prisma } from "@prisma/client"
-
-export class Bairro implements Prisma.BairroCreateInput {
-
-    //Definição de atributo básico
-    nome: string;
-
-    //Declara que a entidade será refereciada como chave estrangeira
-    enderecos?: Prisma.EnderecoUncheckedCreateNestedManyWithoutBairroInput;
+export class Bairro {
+    constructor(
+        public readonly nome: string,
+        public readonly id?: number,
+    ) { }
 }

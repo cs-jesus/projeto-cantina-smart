1b7c37f334586aad37f773bccb25552f1f044baa
Caue Jesus <cs.jesus98@gmail.com>
2024-07-12 12:57:25 -0300

    feat(infra/module): Implementação inicial do módulo cidade

---
+++ src/infraestructure/module/cidade.module.ts
@@ -0,0 +1,24 @@
+import { Module } from "@nestjs/common";
+import { PrismaModule } from "../prisma/prisma.module";
+
+import { CidadeService } from "src/domain/service/cidade.service";
+import { CidadeController } from "src/interface/controllers/cidade.controller";
+import { PrismaCidadeRepository } from "../repositories/prisma/prisma-cidade.repository";
+
+@Module({
+    imports: [PrismaModule],
+    exports: [CidadeService],
+    controllers: [CidadeController],
+
+    providers: [
+        CidadeService,
+
+        
+
+        {
+            provide: 'BCidadeRepository',
+            useClass: PrismaCidadeRepository
+        },
+    ],
+
+}) export class CidadeModule { }

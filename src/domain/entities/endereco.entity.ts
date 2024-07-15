16d37c03f1efc30dd3119fce848b367d556b73d0
Caue Jesus <cs.jesus98@gmail.com>
2024-07-15 09:08:05 -0300

    refactor(entity): Ajuste no modelo de entidade "Endereço"

--- src/domain/entities/endereco.entity.ts
+++ src/domain/entities/endereco.entity.ts
@@ -1,22 +1,21 @@
-import { Prisma } from "@prisma/client";
+import { Logradouro } from './logradouro.entity';
+import { Bairro } from './bairro.entity';
+import { Cidade } from './cidade.entity';
+import { Estado } from './estado.entity';
 
-export class Endereco implements Prisma.EnderecoCreateInput {
 
-    //Definição de atributo básico
-    fk_logradouro: number;
-    fk_bairro: number;
-    fk_cidade: number;
-    fk_estado: number;
-    cep: string;
-    numero: string;
-
-    //Definição de chave estrangeira
-    logradouro: Prisma.LogradouroCreateNestedOneWithoutEnderecosInput;
-    bairro: Prisma.BairroCreateNestedOneWithoutEnderecosInput;
-    cidade: Prisma.CidadeCreateNestedOneWithoutEnderecosInput;
-    estado: Prisma.EstadoCreateNestedOneWithoutEnderecosInput;
-
-    //Declara que a entidade será refereciada como chave estrangeira
-    polos?: Prisma.PoloUncheckedCreateNestedManyWithoutEnderecoInput;
-    filiais?: Prisma.FilialUncheckedCreateNestedManyWithoutEnderecoInput;
+export class Endereco {
+    constructor(
+        public readonly fk_logradouro: number,
+        public readonly fk_bairro: number,
+        public readonly fk_cidade: number,
+        public readonly fk_estado: number,
+        public readonly cep: string,
+        public readonly numero: string,
+        public readonly id?: number,
+        public readonly logradouro?: Logradouro,
+        public readonly bairro?: Bairro,
+        public readonly cidade?: Cidade,
+        public readonly estado?: Estado,
+    ) { }
 }

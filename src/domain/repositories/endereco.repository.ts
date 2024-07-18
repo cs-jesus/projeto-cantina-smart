92c1c3e3b97712c49eb5a0dc7808848634304c06
Caue Jesus <cs.jesus98@gmail.com>
2024-07-18 14:07:26 -0300

    Fix: correção de escrita

--- src/domain/repositories/endereco.repository.ts
+++ src/domain/repositories/endereco.repository.ts
@@ -1,7 +1,7 @@
 import { Endereco } from '../entities/endereco.entity';
 
 export interface EnderecoRepository {
-    create(endereo: Endereco): Promise<Endereco>;
+    create(endereco: Endereco): Promise<Endereco>;
     update(id: number, endereco: Endereco): Promise<Endereco>;
     delete(id: number): Promise<void>;
     findAll(): Promise<Endereco[]>;
